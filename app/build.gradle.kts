535d4f4fefce109ab294d1971afb8b524067ddce
Kirill Menzurenko <menzurenko1998@gmail.com>
2023-12-17 22:30:11 +0300

    add jacoco plugins

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("java")
     id ("checkstyle")
+    id("jacoco")
 }
 
 group = "hexlet.code"
@@ -8,6 +9,10 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+}
+jacoco {
+    toolVersion = "0.8.9"
+    reportsDirectory.set(layout.buildDirectory.dir("customJacocoReportDir"))
 }
 
 dependencies {
